aa620dddf51b226c43b97b891e720f4fae783aa2
Robby Stokoe <robbystk@gmail.com>
2019-03-05 07:43:28 -0700

    Initial functional tests for layout & styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -110,6 +110,29 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # input box centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+            )
+
+        # check for centered-ness after entering an item
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+            )
+
     # also want explanatory text for unique url
     # and retrieval of previously-stored list items
     # She visits that URL - her to-do list is still there.
